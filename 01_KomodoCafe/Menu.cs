60c0276f600e4b14de19e970f79b2e787d9171ce
kyldnewt99 <newky99@gmail.com>
2020-09-30 10:35:30 -0400

    Add project files.

---
+++ 01_KomodoCafe/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_KomodoCafe
+{
+    public class Menu
+    {
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        public double Price { get; set; }
+    }
+}
